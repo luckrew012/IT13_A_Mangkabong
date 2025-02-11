51f0da6f5e0c7d5ee86a64d54ffb8e18a4907cb2
angel <araesjerryco@gmail.com>
2025-02-11 08:38:25 +0800

    Added Java application

---
+++ mangkabong_IT13section_activities_/src/mangkabong_it13section_activities_/Mangkabong_IT13section_activities_.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mangkabong_it13section_activities_;
+
+/**
+ *
+ * @author User
+ */
+public class Mangkabong_IT13section_activities_ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
